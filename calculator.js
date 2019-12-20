18911a495e59914a9cfb8929aa15441c845d5330
Jenna Lovett <jennablair12@gmail.com>
2019-12-19 21:12:29 -0500

    fixed gpa calculation mistake

--- calculator.js
+++ calculator.js
@@ -1,12 +1,14 @@
 function calculateGPA()
 {
-    var grades = document.getElementsByName("grade");
-    var credits = document.getElementsByName("credit");
-    var index;
-    var qualityPoints = 0.0;
-    var semesterCredits = 0.0;
-    var semesterGPA;
+    var grades = document.getElementsByName("grade");   //all grade dropdown boxes
+    var credits = document.getElementsByName("credit"); //all credit textboxes
+    var index;  //also serves as count of number of classes
+    var semesterQualityPoints = 0.0;    //sum of individual numeric grade
+    var semesterCredits = 0.0;  //total credits of semester classes
+    var semesterGPA;    //semester GPA
 
+    //go through each grade/credit and convert letter grade to numeric grade
+    //sum quality points and credits
     for(index = 0; index < grades.length; ++index)
     {
         var letterGrade = grades[index].options[grades[index].selectedIndex].value;
@@ -31,8 +33,18 @@ function calculateGPA()
                 grade = 0.0;
                 break;
         }
-        qualityPoints += parseFloat(grade, 10);
+        semesterQualityPoints += parseFloat(grade, 10) * parseFloat(credit, 10);
         semesterCredits += parseFloat(credit, 10);
     }
-    semesterGPA = qualityPoints / parseFloat(index, 10);
+    //calculate semester GPA
+    semesterGPA = semesterQualityPoints / parseFloat(semesterCredits, 10).toFixed(2);
+
+    //calculate cumulative GPA
+    //cGPA = qualityPoints / totalCredits
+    //qualityPoints = cGPA * totalCredits
+}
+
+function calculateCumulativeGPA()
+{
+
 }
